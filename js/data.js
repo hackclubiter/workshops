056f9d80a4324563438a99ee9598a5dfdbeb16df
saswatsamal <samalsaswat0@gmail.com>
2021-08-22 10:58:13 +0530

    [add] : Past Workshop Details | More changes too

--- js/data.js
+++ js/data.js
@@ -1,11 +1,61 @@
 const workshopData = [
-    {
-        workshopName: "Name of the workshop",
-        about: "About the cool workshop.", //About the workshop in about 50 words
-        link: "https://iter.hackclub.com/workshops", //enter the link of the workshop
-        image: "https://images.unsplash.com/photo-1542626991-cbc4e32524cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d29ya3Nob3B8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", //give the image (make sure to host it before adding)
-    },
-    
+    // {
+    //     workshopName: "Name of the workshop",
+    //     about: "About the cool workshop.", //About the workshop in about 50 words
+    //     link: "https://iter.hackclub.com/workshops", //enter the link of the workshop
+    //     image: "https://images.unsplash.com/photo-1542626991-cbc4e32524cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d29ya3Nob3B8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", //give the image (make sure to host it before adding)
+    // },
 ];
 
+//Past
 
+const pastWorkshopData = [
+    {
+        workshopName: "Python Development",
+        about: "Learn the basics of Python in 7 Days", //About the workshop in about 50 words
+        link: "https://github.com/hackclubiter/Codathon-PyDev", //enter the link of the workshop
+        image: "https://images.unsplash.com/photo-1542626991-cbc4e32524cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d29ya3Nob3B8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", //give the image (make sure to host it before adding)
+    },
+    {
+        workshopName: "Web Development",
+        about: "Learn the basics of web in 7 Days", //About the workshop in about 50 words
+        link: "https://github.com/hackclubiter/Codathon-WebDev", //enter the link of the workshop
+        image: "https://images.unsplash.com/photo-1542626991-cbc4e32524cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d29ya3Nob3B8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", //give the image (make sure to host it before adding)
+    },
+    {
+        workshopName: "Decode the Programming with Shubham Sonal",
+        about: "How to get started with programming!", //About the workshop in about 50 words
+        link: "https://youtu.be/nHzmwoStZKU", //enter the link of the workshop
+        image: "https://i.ytimg.com/an_webp/nHzmwoStZKU/mqdefault_6s.webp?du=3000&sqp=COith4kG&rs=AOn4CLD_twEQNf26fgNMzaS0YHM3uKF78Q", //give the image (make sure to host it before adding)
+    },
+    {
+        workshopName: "Hack the Web w/ Harsh Bajpai",
+        about: "Harsh gave us an overall idea about Web-Dev!", //About the workshop in about 50 words
+        link: "https://youtu.be/7K0OLLjq0DY", //enter the link of the workshop
+        image: "https://i.ytimg.com/vi/7K0OLLjq0DY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAPSXqrTYqHY4zQNYBgiXNh2k6Kag", //give the image (make sure to host it before adding)
+    },
+    {
+        workshopName: "Codathon",
+        about: "An amalgamation of Python development along with Web development.", //About the workshop in about 50 words
+        link: "https://youtu.be/ERhCWZQ6GXY", //enter the link of the workshop
+        image: "https://i.ytimg.com/vi/ERhCWZQ6GXY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBU5e3kkBihfRDSaG2XG00SXrEGuQ", //give the image (make sure to host it before adding)
+    },
+    {
+        workshopName: "Ask Samarjeet",
+        about: "Regarding Internships and preparation.", //About the workshop in about 50 words
+        link: "https://youtu.be/x_LF5XpcUAQ", //enter the link of the workshop
+        image: "https://i.ytimg.com/vi/x_LF5XpcUAQ/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA8I7yPcdCJkqRrHaPFAOunuLKvZA", //give the image (make sure to host it before adding)
+    },
+    {
+        workshopName: "What is IFTTT?",
+        about: "Learn about automation & IOT at one place.", //About the workshop in about 50 words
+        link: "https://youtu.be/8JqAPCWBh2E", //enter the link of the workshop
+        image: "https://i.ytimg.com/vi/8JqAPCWBh2E/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCboA22hRDTXsILUO9ubUmdeNuJ9w", //give the image (make sure to host it before adding)
+    },
+    {
+        workshopName: "What is IoT?",
+        about: "Get started with IoT", //About the workshop in about 50 words
+        link: "https://youtu.be/mNMFa6tL5PE", //enter the link of the workshop
+        image: "https://i.ytimg.com/vi/mNMFa6tL5PE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDnb0kFHtpwnoLLeiGk1zphWXM4kg", //give the image (make sure to host it before adding)
+    },
+];
--- js/index.js
+++ js/index.js
@@ -1,19 +1,41 @@
 function workshopDisplay(workshop) {
     return `
-   
-  <li>
-      <div class="ws-box" style="background-image: url('${workshop.image}');">
-      <a href="${workshop.link}" class="wslink">
-          <div class="ws-info">
-              <h3>${workshop.workshopName}</h3>
-              <p>${workshop.about}</p>
-          </div>
-      </a>
-      </div>
-  </li>
-  `;
-  }
+        <li>
+            <div class="ws-box" style="background-image: url('${workshop.image}');">
+            <a href="${workshop.link}" class="wslink">
+                <div class="ws-info">
+                    <h3>${workshop.workshopName}</h3>
+                    <p>${workshop.about}</p>
+                </div>
+            </a>
+            </div>
+        </li>
+        `;
+    }
   
-  document.getElementById("app").innerHTML = `
+  document.getElementById("upcoming").innerHTML = `
   ${workshopData.map(workshopDisplay).join("")}
-  `;
+
+`;
+
+//Past Workshops
+
+function pastWorkshops(workshop) {
+    return `
+        <li>
+            <div class="ws-box" style="background-image: url('${workshop.image}');">
+            <a href="${workshop.link}" class="wslink">
+                <div class="ws-info">
+                    <h3>${workshop.workshopName}</h3>
+                    <p>${workshop.about}</p>
+                </div>
+            </a>
+            </div>
+        </li>
+        `;
+    }
+  
+  document.getElementById("past").innerHTML = `
+  ${pastWorkshopData.map(pastWorkshops).join("")}
+  <p>There are ${pastWorkshopData.length} workshops.</p>
+`;
