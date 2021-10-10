2356ece1025aedfd8f226819f08b6e143004208e
Saswat Mohanty <mohantysaswat154@gmail.com>
2021-10-10 18:33:41 +0530

    updated future events

--- js/data.js
+++ js/data.js
@@ -6,11 +6,27 @@ const workshopData = [
     //     image: "https://images.unsplash.com/photo-1542626991-cbc4e32524cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d29ya3Nob3B8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", //give the image (make sure to host it before adding)
     // },
     {
-        workshopName: "Hack the October Kick Off",
-        about: "Opening Ceremony of Hack the October", //About the workshop in about 50 words
-        link: "https://www.youtube.com/watch?v=QPssaIng13U", //enter the link of the workshop
+        workshopName: "AMA on Flutter and Open-Source",
+        about: "AMA session with Himanshu Sharma", //About the workshop in about 50 words
+        link: "#", //enter the link of the workshop
         image: "https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHdvcmtzaG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", //give the image (make sure to host it before adding)
-        date: "01 Oct 2021", //give the event's date
+        date: "17 Oct 2021", //give the event's date
+        time: "7 PM Onwards", //give the event's time
+    },
+    {
+        workshopName: "AMA on GitHub Externship",
+        about: "AMA session with Saswat Mishra", //About the workshop in about 50 words
+        link: "#", //enter the link of the workshop
+        image: "https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHdvcmtzaG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", //give the image (make sure to host it before adding)
+        date: "18 Oct 2021", //give the event's date
+        time: "7 PM Onwards", //give the event's time
+    },
+    {
+        workshopName: "AMA on Web-Dev and Open-Source",
+        about: "AMA session with Souvik Kar", //About the workshop in about 50 words
+        link: "#", //enter the link of the workshop
+        image: "https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHdvcmtzaG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", //give the image (make sure to host it before adding)
+        date: "24 Oct 2021", //give the event's date
         time: "7 PM Onwards", //give the event's time
     },
 ];
@@ -19,29 +35,49 @@ const workshopData = [
 
 const pastWorkshopData = [
     {
+        workshopName: "Getting Started with GitHub",
+        about: "Workshop on basics of GitHub", //About the workshop in about 50 words
+        link: "#", //enter the link of the workshop
+        image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1352&q=80", //give the image (make sure to host it before adding)
+    },
+    {
+        workshopName: "AMA on Open-Source and Communities",
+        about: "AMA session with Kartikey Rawat", //About the workshop in about 50 words
+        link: "https://youtu.be/rSoRhwamoYg", //enter the link of the workshop
+        image: "https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHdvcmtzaG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", //give the image (make sure to host it before adding)
+    },
+    {
+        workshopName: "AMA on Hackathons, Communities and Open source",
+        about: "AMA session with Siddharth Dayalwal", //About the workshop in about 50 words
+        link: "https://youtu.be/Zwe-Q9PRToA", //enter the link of the workshop
+        image: "https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHdvcmtzaG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", //give the image (make sure to host it before adding)
+    },
+    {
+        workshopName: "Hack the October Kick Off",
+        about: "Opening Ceremony of Hack the October", //About the workshop in about 50 words
+        link: "https://www.youtube.com/watch?v=QPssaIng13U", //enter the link of the workshop
+        image: "https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHdvcmtzaG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", //give the image (make sure to host it before adding)
+    },
+    {
         workshopName: "2021-22 Session Induction",
         about: "Induction program for the 2022 session of HCI.", //About the workshop in about 50 words
-        link: "https://lu.ma/b0u7uhqm", //enter the link of the workshop
+        link: "https://youtu.be/gfQpugUrtkc", //enter the link of the workshop
         image: "https://images.unsplash.com/photo-1577113398331-d843d3341a63?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80", //give the image (make sure to host it before adding)
-        date: "10 Sep 2021", //give the event's date
-        time: "7 PM Onwards", //give the event's time
     },
     {
         workshopName: "Getting Started with Arduino",
         about: "Build your first project with Arduino Uno and we will help in Hardware.", //About the workshop in about 50 words
         link: "#", //enter the link of the workshop
         image: "https://images.unsplash.com/photo-1559819614-81fea9efd090?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80", //give the image (make sure to host it before adding)
-        date: "15 Sep 2021", //give the event's date
-        time: "7 PM Onwards", //give the event's time
     },
-    {
-        workshopName: "Build your Wikipedia Summarizer",
-        about: "Make your Wikipedia Summarizer GUI using Python.", //About the workshop in about 50 words
-        link: "#", //enter the link of the workshop
-        image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1352&q=80", //give the image (make sure to host it before adding)
-        date: "25 Sep 2021", //give the event's date
-        time: "7 PM Onwards", //give the event's time
-    },
+    // {
+    //     workshopName: "Build your Wikipedia Summarizer",
+    //     about: "Make your Wikipedia Summarizer GUI using Python.", //About the workshop in about 50 words
+    //     link: "#", //enter the link of the workshop
+    //     image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1352&q=80", //give the image (make sure to host it before adding)
+    //     date: "25 Sep 2021", //give the event's date
+    //     time: "7 PM Onwards", //give the event's time
+    // },
     {
         workshopName: "Python Development",
         about: "Learn the basics of Python in 7 Days", //About the workshop in about 50 words
